65853a7bea57ce88228cbe7a06a588a62ea0475e
Yokoberek <salupacode@gmail.com>
2026-05-27 12:59:36 +0900

    fix(settings): add EMAIL_USE_SSL config option

--- django_project/settings/base.py
+++ django_project/settings/base.py
@@ -121,6 +121,7 @@ EMAIL_BACKEND = config(
 EMAIL_HOST = config("EMAIL_HOST", default="localhost")
 EMAIL_PORT = config("EMAIL_PORT", default=25, cast=int)
 EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=False, cast=bool)
+EMAIL_USE_SSL = config("EMAIL_USE_SSL", default=False, cast=bool)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
 DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default="noreply@papuanthought.com")
