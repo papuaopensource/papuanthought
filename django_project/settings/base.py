855cc25dfb663f994f55c848735e06bcdc882e34
Yokoberek <salupacode@gmail.com>
2026-05-27 12:16:04 +0900

    fix(settings): update MEDIA_ROOT path to use 'mediafiles' directory

--- django_project/settings/base.py
+++ django_project/settings/base.py
@@ -102,7 +102,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
