8eea3d2e6354e8d4f4348cd5ff1d38b54920e529
Yokoberek <salupacode@gmail.com>
2026-05-27 12:46:52 +0900

    fix(settings): update DEFAULT_FROM_EMAIL domain to papuanthought.com

--- django_project/settings/base.py
+++ django_project/settings/base.py
@@ -120,7 +120,7 @@ EMAIL_PORT = config("EMAIL_PORT", default=25, cast=int)
 EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=False, cast=bool)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
-DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default="noreply@papuanthought.org")
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default="noreply@papuanthought.com")
 
 UNFOLD = {
     "SITE_TITLE": "The Papuan Thoughts Project",
